242f71252eb7e13315820e436d13a81b880c0af0
juned <147612758+ndlewer@users.noreply.github.com>
2025-09-13 12:21:54 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-let rootDomain = "";
-let serviceName = "";
+let rootDomain = "emboehrareti.workers.dev";
+let serviceName = "rusak";
 let APP_DOMAIN = "";
 
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
